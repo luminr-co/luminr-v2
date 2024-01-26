95727a68120bda0e2abe3aad58fc170bbccb3497
Samriddhi Paudel <paudelsamriddhi04@gmail.com>
2024-01-26 11:56:42 +0545

    image change in work section

--- src/modules/home/presentation/components/WorkSection/WorkSection.tsx
+++ src/modules/home/presentation/components/WorkSection/WorkSection.tsx
@@ -3,19 +3,21 @@ import { SliderEntity } from "@/core/domain/entities/SliderEntity";
 import tiggImage from "@/core/presentation/assets/images/work/tiggapp.webp";
 import { SectionContainer } from "@/core/presentation/components/SectionContainer";
 import { Slider } from "@/core/presentation/components/Slider";
-import brandpark from "@/core/presentation/assets/images/work/brandpark.png";
-import chewybliss from "@/core/presentation/assets/images/work/chewybliss.png";
+import brandpark from "@/core/presentation/assets/images/work/brandpark.webp";
+import vista_mails from "@/core/presentation/assets/images/work/vista-mails.webp";
 
 export default function WorkSection() {
   const works: SliderEntity[] = [
     {
-      image: tiggImage,
+      image: vista_mails,
     },
+
     {
       image: brandpark,
     },
+
     {
-      image: chewybliss,
+      image: tiggImage,
     },
   ];
 
